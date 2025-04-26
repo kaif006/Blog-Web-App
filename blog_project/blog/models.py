5ae6b82c1bdfd623a0ac7cc539a851f8c5056002
kaif006 <kaifjallalani@gmail.com>
2025-04-26 14:30:50 +0500

    initial database setup

---
+++ blog_project/blog/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    categories = models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tag)
+    featured_image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    slug = models.SlugField(unique=True, max_length=200)
+    views = models.PositiveIntegerField(default=0)
+    is_published = models.BooleanField(default=False)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.is_published = True
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
